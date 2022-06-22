01da7586a4d2f421cd6bf133e82eaef4a2c8841e
LucianoIsgro <matias.storoni@gmail.com>
2022-06-22 20:04:21 -0300

    Hice mal otra test

--- src/Clases/Domicilio.java
+++ src/Clases/Domicilio.java
@@ -1,7 +1,7 @@
 package Clases;
 
 public class Domicilio {
-	public static String codigoPostal= "4579";
+	public static String codigoPostal= "45792";
 	public static String calle = "San Martin";
 	public static String numero = "756";
 	public static String departamento = "5A";
